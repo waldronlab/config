0682a9e24b9aa0277124d93a0732c182cac46789
LiNk-NY <mramos09@gmail.com>
2018-07-18 18:34:43 -0400

    add version_bump script

--- copyCONFIG.sh
+++ copyCONFIG.sh
@@ -8,6 +8,13 @@ if [[ ${1} == "README.md" || ${1} == "copyCONFIG.sh" ]]; then
     echo "Nothing to do with ${1}"
 elif [[ ${1} == "ssh-slash-config" ]]; then
     cp -vn ./${1} $HOME/.ssh/config
+elif [[ ${1} == "version_bump.sh" ]]; then
+    mkdir -p $HOME/src
+    cp -vn ./${1} $HOME/src
+    if [ ! -f $HOME/.bash_aliases ]; then
+        touch $HOME/.bash_aliases
+    fi
+    echo "alias bump='/home/$USER/src/version_bump.sh" >> $HOME/.bash_aliases
 else
     cp -vn ./${1} $HOME/.${1}
 fi
