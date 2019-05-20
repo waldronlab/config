078bdf1ac9486114464051642ad6f6d8de99522d
LiNk-NY <mramos09@gmail.com>
2019-05-20 10:55:37 -0400

    updates to test_pkg

--- TEST_pkg.sh
+++ TEST_pkg.sh
@@ -4,15 +4,15 @@ pkg=$1
 
 for name in devel release oldrel
 do
-    r_loc=$HOME/src/svn/r-$name/R/bin/R/
-    lib_dir=$HOME/src/lib/r-$name
+    R_LOC=$HOME/src/svn/r-${name}/R/bin/R
+    LIB_DIR=$HOME/src/lib/r-${name}
     if [ ! -d $lib_dir ]; then
         mkdir -p $lib_dir
     fi
     echo "Testing $pkg on r-${name}..."
-    R_LIBS_USER=$lib_dir $r_loc --vanilla CMD build $pkg
+    R_LIBS_USER=$LIB_DIR $R_LOC --vanilla CMD build $pkg
     echo "Checking $pkg on r-${name}"
-    R_LIBS_USER=$lib_dir $r_loc --vanilla CMD check ${pkg}_*.tar.gz
+    R_LIBS_USER=$LIB_DIR $R_LOC --vanilla CMD check ${pkg}_*.tar.gz
 done
 
 
