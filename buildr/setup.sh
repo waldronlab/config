549750b76c2e9bec6f322d3f28e36626aed2361a
LiNk-NY <marcel.ramosperez@roswellpark.org>
2021-10-04 14:01:53 -0400

    add info for additional packages if clean install

--- buildr/setup.sh
+++ buildr/setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+## see also
+## https://github.com/Jiefei-Wang/Painless-R-compilation-and-installation-on-Ubuntu
+
 ## install dependencies
-apt-get build-dep r-base
-apt-get install subversion ccache
+apt-get build-dep r-base r-base-core
+apt-get install
+apt-get install make subversion ccache build-essential libreadline-dev \
+gfortran xorg-dev libbz2-dev liblzma-dev libpcre2-dev \
+libcurl4 libcurl4-openssl-dev libblas-dev libtirpc-dev
 
